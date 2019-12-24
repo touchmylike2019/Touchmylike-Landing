661ba48d0c15b935620862f6e8a9a1ae8a8408d2
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2019-12-25 00:49:30 +0700

    add page happy new year blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,29 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import Header from '../components/Header/Header'
 import { Container } from 'reactstrap'
+import bestculling from '../images/bestculling.jpg'
 
 export default () => {
     return (
         <Fragment>
             <Header />
             <Container>
-                <div className="layer">
-                    <h1>บทความ</h1>
+                <div className="blog">
+                    <h1>Happy New Year 2020</h1>
+                    <div className="profile">
+                        <img src={bestculling} className="avatar" />
+                        <span>BESTCULLING | DECEMBER 25, 2019</span>
+                    </div>
+                    <p>
+                        With the Stencil One release behind us, we’re seeing developers build lots of successful Stencil-based web components. Now that the dust has settled, we realized it was time to talk about what’s next for the future of the project.
+                    </p>
+                    <Link to="/">
+                            อ่านเพิ่มเติม
+                    </Link>
                 </div>
             </Container>
+
         </Fragment>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,28 @@
 import React, { Fragment } from 'react'
 import Header from '../components/Header/Header'
-import { 
+import {
     Container,
-    Button
+    Button,
+    Alert
 } from 'reactstrap'
 
 export default () => {
     return (
         <Fragment>
             <Header />
-            <Container>
-                <div className="layer">
-                    <h1 style={{fontWeight: "bold"}}>Leaning skill technology</h1>
+            <Container className="mt-3">
+                <Alert color="primary">
+                    Blog: Happy New Year 2020 >
+                </Alert>
+                <div className="layer mt-4">
+                    <h1 style={{ fontWeight: "bold" }}>Leaning skill technology</h1>
                     <p className="mt-4">
                         ในโลกปัจจุบันเทคโนโลยีนั้นเปลี่ยนอยู่ทุกวัน ฉะนั้นเเล้วเราจึงต้องพัฒนาทักษะตนเองอยู่เสมอ เพื่อพร้อมต่อการเปลี่ยนเเปลงของโลกยุคใหม่
                     </p>
-                    <Button color="primary" className="mt-4 p-3" style={{width: "100%", fontWeight: "bold"}}>เริ่มกันเลย</Button>
-                    <Button className="mt-4 p-3" 
-                        style={{color: "rgb(76,72,255)", width: "100%", background: "#FFF", border: "none", boxShadow: "0 0 5px rgba(0, 0, 0, 0.15)", fontWeight: "bold"}}>
-                            Touchmylike คืออะไร?
+                    <Button color="primary" className="mt-4 p-3" style={{ width: "100%", fontWeight: "bold" }}>เริ่มกันเลย</Button>
+                    <Button className="mt-4 p-3"
+                        style={{ color: "rgb(76,72,255)", width: "100%", background: "#FFF", border: "none", boxShadow: "0 0 5px rgba(0, 0, 0, 0.15)", fontWeight: "bold" }}>
+                        Touchmylike คืออะไร?
                     </Button>
                 </div>
             </Container>
