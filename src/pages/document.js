ba6be57e877a963c6c8fd49d99d2bbc4ab484ceb
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2019-12-25 14:44:01 +0700

    add card in docs page

--- src/pages/document.js
+++ src/pages/document.js
@@ -1,14 +1,43 @@
 import React, { Fragment } from 'react'
 import Header from '../components/Header/Header'
-import { Container } from 'reactstrap'
+import {
+    Container,
+    Row, Col,
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Button
+} from 'reactstrap'
+import cpp from '../images/cpp.png'
+import sfml from '../images/sfml.png'
 
 export default () => {
     return (
         <Fragment>
             <Header />
             <Container>
-                <div className="layer">
+                <div className="docs">
                     <h1>เอกสาร</h1>
+                    <Row>
+                        <Col xs="12">
+                            <Card className="mt-4">
+                                <CardImg top width="100%" src={cpp} alt="C++" />
+                                <CardBody>
+                                    <CardTitle className="font-weight-bold">C++</CardTitle>
+                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                    <Button>อ่านเพิ่มเติม</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col xs="12">
+                            <Card className="mt-4">
+                                <CardImg top width="100%" src={sfml} alt="sfml" />
+                                <CardBody>
+                                    <CardTitle className="font-weight-bold">SFML</CardTitle>
+                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                    <Button>อ่านเพิ่มเติม</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
                 </div>
             </Container>
         </Fragment>
