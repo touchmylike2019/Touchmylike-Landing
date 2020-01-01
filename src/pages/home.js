a9dd2dc0084ed2240c0e6384c3197572ff67ceed
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2020-01-01 14:44:46 +0700

    add image in home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,6 +7,7 @@ import {
     Button,
     Alert
 } from 'reactstrap'
+import touchmylike from '../images/home_tml.png'
 
 const mapStateToProps = state => {
     return {
@@ -24,11 +25,12 @@ export default connect(mapStateToProps)(({isAuthenticated}) => {
                 </Alert>
                 <div className="layer mt-4">
                     <h1 style={{ fontWeight: "bold" }}>Leaning skill technology</h1>
+                    <img width="100%" src={touchmylike} alt="touchmylike" />
                     <p className="mt-4">
                         ในโลกปัจจุบันเทคโนโลยีนั้นเปลี่ยนอยู่ทุกวัน ฉะนั้นเเล้วเราจึงต้องพัฒนาทักษะตนเองอยู่เสมอ เพื่อพร้อมต่อการเปลี่ยนเเปลงของโลกยุคใหม่
                     </p>
                     <Link to={isAuthenticated ? "/documents" : "/login"}><Button color="primary" className="mt-4 p-3" style={{ width: "100%", fontWeight: "bold" }}>เริ่มกันเลย</Button></Link>
-                    <Link to="/about"><Button className="mt-4 p-3"
+                    <Link to="/about"><Button className="mt-4 mb-5 p-3"
                         style={{ color: "rgb(76,72,255)", width: "100%", background: "#FFF", border: "none", boxShadow: "0 0 5px rgba(0, 0, 0, 0.15)", fontWeight: "bold" }}>
                         Touchmylike คืออะไร?
                     </Button></Link>
