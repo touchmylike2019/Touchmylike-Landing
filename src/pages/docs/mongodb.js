6b45e0278f5b0c8c33908757e7c9b7609bccafd6
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2020-03-17 15:51:26 +0700

    Update 17/03/2020

--- src/pages/docs/mongodb.js
+++ src/pages/docs/mongodb.js
@@ -16,6 +16,23 @@ export default () => {
                     <h3 className="mt-5">พื้นฐานของ MongoDB</h3>
                     <ul className="mt-3">
                         <Link to="/documents/mongodb/overview"><li>เเนะนำ MongoDB เบื้องต้น</li></Link>
+                        <Link to="/"><li>Data Modelling</li></Link>
+                        <Link to="/"><li>Create Database</li></Link>
+                        <Link to="/"><li>Drop Database</li></Link>
+                        <Link to="/"><li>Create Collection</li></Link>
+                        <Link to="/"><li>Drop Collection</li></Link>
+                        <Link to="/"><li>Datatypes</li></Link>
+                        <Link to="/"><li>Insert Document</li></Link>
+                        <Link to="/"><li>Query Document</li></Link>
+                        <Link to="/"><li>Update Document</li></Link>
+                        <Link to="/"><li>Delete Document</li></Link>
+                        <Link to="/"><li>Projection</li></Link>
+                        <Link to="/"><li>Limit Records</li></Link>
+                        <Link to="/"><li>Sort Records</li></Link>
+                        <Link to="/"><li>Indexing</li></Link>
+                        <Link to="/"><li>Aggregation</li></Link>
+                        <Link to="/"><li>Replication</li></Link>
+                        <Link to="/"><li>Sharding</li></Link>
                     </ul>
                 </div>
             </Container>
